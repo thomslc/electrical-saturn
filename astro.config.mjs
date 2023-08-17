ebf867a837bd304bcce2600ca1b740f0390aa939
thomslc <142228116+thomslc@users.noreply.github.com>
2023-08-17 16:59:06 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astronaut.github.io',
-  base: '/my-repo',
+  site: 'https://thomslc.github.io',
+  base: '/electrical-saturn',
 });
